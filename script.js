b282b71b984bf2d2d905eae754c1ac64b9568228
gtejakrishna2000 <85777606+gtejakrishna2000@users.noreply.github.com>
2023-10-21 22:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let arr = [];
+	let c = 0;
+  while(num>0){
+	  let rem = num%2;
+	  arr[c++] = rem;
+	  num = num/2;
+  }
+	return c;
 }
 
 window.decimalToBinary = decimalToBinary;
